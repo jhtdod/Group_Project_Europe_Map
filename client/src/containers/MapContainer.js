0bc3f03266ab1b1249c783a389051a32c3cd1203
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-21 13:30:01 +0000

    Created basic geochart for europe

--- client/src/containers/MapContainer.js
+++ client/src/containers/MapContainer.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Chart } from "react-google-charts";
 import CountryCard from '../components/CountryCard';
 import CountryList from '../components/CountryList';
 import CountrySearch from '../components/CountrySearch';
+import {europe_data, europe_options, micro_europe_data, micro_europe_options} from './EuropeData';
 
 const MapContainer = () => {
 
     return (
         <>
             <h2>Map Container</h2>
+            <Chart
+                className="map"
+                chartType="GeoChart"
+                width="85%"
+                height="75%"
+                data={europe_data}
+                options={europe_options}
+            />
             <CountryList/>
             <CountryCard/>
             <CountrySearch/>
