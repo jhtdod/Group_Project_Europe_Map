1d2ec251543fa684a4cc9d9bc369f68598c20374
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-22 16:21:30 +0000

    Removed zoom controls for map

--- client/src/containers/LeafletMap.js
+++ client/src/containers/LeafletMap.js
@@ -6,7 +6,7 @@ const LeafletMap = () => {
 
     return (
             <div className="leaflet-container">
-            <MapContainer center={[52, 27.5]} zoom={3.9}>
+            <MapContainer center={[52, 27.5]} zoom={3.9} zoomControl={false} scrollWheelZoom={false} doubleClickZoom= {false} closePopupOnClick= {false} dragging= {false} zoomSnap= {false} zoomDelta= {false} trackResize= {false} touchZoom= {false} scrollWheelZoom= {false}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
