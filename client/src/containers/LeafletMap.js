cf9c0851530a86c806f301c1b615ffd65a520177
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-22 21:50:52 +0000

    Styling for GeoJSON layer

--- client/src/containers/LeafletMap.js
+++ client/src/containers/LeafletMap.js
@@ -5,13 +5,21 @@ import europe_json from "./NewEurope.json";
 
 const LeafletMap = ({setSelectedCountry}) => {
 
+    const countryStyle = {
+        fillColor: "#7eca7e",
+        fillOpacity: 0.3,
+        color: "black",
+        weight: 1
+    };
+
     return (
             <div className="leaflet-container">
             <MapContainer center={[50, 28]} zoom={4} attributionControl={false} zoomControl={false} scrollWheelZoom={false} doubleClickZoom= {false} closePopupOnClick= {false} dragging= {false} zoomSnap= {false} zoomDelta= {false} trackResize= {false} touchZoom= {false} scrollWheelZoom= {false}>
                 <TileLayer
                 url={env.MAP_TILESET_URL}
                 />
-                <GeoJSON 
+                <GeoJSON
+                style={countryStyle} 
                 data={europe_json}
                 />
             </MapContainer>
