958c1733f95d08acc98490e27a53819f4f72da27
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-22 18:13:23 +0000

    Installed reactenv and changed api key

--- client/src/containers/LeafletMap.js
+++ client/src/containers/LeafletMap.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import {Icon, map} from 'leaflet';
+import env from 'react-dotenv';
 
 const LeafletMap = () => {
 
     return (
             <div className="leaflet-container">
-            <MapContainer center={[52, 27.5]} zoom={3.9} zoomControl={false} scrollWheelZoom={false} doubleClickZoom= {false} closePopupOnClick= {false} dragging= {false} zoomSnap= {false} zoomDelta= {false} trackResize= {false} touchZoom= {false} scrollWheelZoom= {false}>
-                <TileLayer
+            <MapContainer center={[50, 28]} zoom={4} attributionControl={false} zoomControl={false} scrollWheelZoom={false} doubleClickZoom= {false} closePopupOnClick= {false} dragging= {false} zoomSnap= {false} zoomDelta= {false} trackResize= {false} touchZoom= {false} scrollWheelZoom= {false}>
+                {/* <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                /> */}
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url={env.MAP_TILESET_URL}
                 />
             </MapContainer>
             </div>
