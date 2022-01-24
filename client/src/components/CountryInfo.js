8d6d9d2787f21c89985a3afbf1364beda3f3e480
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-24 16:45:20 +0000

    Added info to countrycard

--- client/src/components/CountryInfo.js
+++ client/src/components/CountryInfo.js
@@ -7,7 +7,14 @@ const CountryInfo = ({countryInfo}) => {
 
     return (
 
-        <>Country Info</>
+        <>
+        <img src={countryInfo[1].flags.png} width="100" alt={`Flag of ${countryInfo[0].country}`}/>
+        <p>Capital: {countryInfo[1].capital}</p>
+        <p>Population: {countryInfo[1].population}</p>
+        <p>Hello: {countryInfo[0].greeting}</p>
+        <p>National Animal: {countryInfo[0].animal}</p>
+        <p>National Dish: {countryInfo[1].dish}</p>
+        </>
         
     )
 
