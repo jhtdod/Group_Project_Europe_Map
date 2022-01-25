90e4faf18131c5158981ce0808b1df9a1958d1c2
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-25 12:02:20 +0000

    Added quiz form

---
+++ client/src/components/CountryQuiz.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CountryQuiz = ({countryList}) => {
+
+    
+
+    return (
+        <>
+        <form>
+            <input type="text"/>
+            <input type="submit"/>
+        </form>
+        </>
+    )
+}
+
+export default CountryQuiz;
