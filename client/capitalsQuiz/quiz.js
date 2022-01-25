e4c6bd6ad058670ec43d5786bd2091a925bda72c
garyb6 <gary.bennett6@outlook.com>
2022-01-25 10:49:36 +0000

    timer inserted

--- client/capitalsQuiz/quiz.js
+++ client/capitalsQuiz/quiz.js
@@ -56,6 +56,7 @@ const questions = [
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', countdown)
 
 function startGame () {
     startButton.classList.add('hide')
@@ -130,3 +131,43 @@ function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
+
+let start = Date.now(),
+    diff,
+    minutes,
+    seconds;
+
+
+function startTimer(duration, display) {
+
+    
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+
+function timer() {
+    // get the number of seconds that have elapsed since 
+    // startTimer() was called
+    diff = duration - (((Date.now() - start) / 1000) | 0);
+
+    minutes = (diff / 60) | 0;
+    seconds = (diff % 60) | 0;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds; 
+
+    if (diff <= 0) {
+        // add one second so that the count down starts at the full duration
+        // example 05:00 not 04:59
+        start = Date.now() + 1000;
+    }
+};
+}
+
+function countdown (){
+    var tenMinutes = 60 * 10,
+        display = document.querySelector('#time');
+    startTimer(tenMinutes, display);
+};
