dcc91d2f87398c354b49be4171d05273f49b6b74
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-25 12:14:04 +0000

    Added some state handling functions

--- client/src/components/CountryQuiz.js
+++ client/src/components/CountryQuiz.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CountryQuiz = ({countryList}) => {
 
-    
+    const [answer, setAnswer] = useState("");
+    const [formData, setFormData] = useState({})
+
+    const handleChange = (event) =>{
+        formData[event.target.id] = event.target.value;
+        setFormData(formData);
+    }
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        setAnswer(formData.answer)
+    }
 
     return (
         <>
-        <form>
-            <input type="text"/>
+        <form onSubmit={onSubmit}>
+            <input type="text" id="answer" onChange={handleChange}/>
             <input type="submit"/>
         </form>
         </>
