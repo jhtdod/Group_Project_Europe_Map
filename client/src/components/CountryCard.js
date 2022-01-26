a64117e49c02181ee8abbb83f39ba2c64506db19
garyb6 <gary.bennett6@outlook.com>
2022-01-26 00:01:04 +0000

    css country card and title updated

--- client/src/components/CountryCard.js
+++ client/src/components/CountryCard.js
@@ -1,22 +1,22 @@
 import React from "react";
 import CountryInfo from "./CountryInfo";
-import Card from 'react-bootstrap/Card';
-import CloseButton from "react-bootstrap/esm/CloseButton";
+import {Card, CloseButton, Container} from 'react-bootstrap';
+import "./CountryCard.css"
 
 const CountryCard = ({ selectedCountry, countryInfo, handleClose }) => {
     return (
-        <>
-            <Card style={{width: '100%', height: '100%'}}>
-                <Card.Header as='h4'>{selectedCountry} 
-                <CloseButton onClick={handleClose}/>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <CountryInfo countryInfo={countryInfo}/>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </>
+
+        <Container>
+                <Card  text="light" bg="dark" border="dark" style={{width: '100%', height: '100%'}}>
+                <CloseButton variant="white" onClick={handleClose} aria-label="hide"/>
+                    <Card.Body>
+                    <Card.Title as='h4'>{selectedCountry}</Card.Title>
+                                    <Card.Text>
+                                    <p><CountryInfo countryInfo={countryInfo}></CountryInfo></p>
+                                    </Card.Text>
+                    </Card.Body>
+                </Card>
+        </Container>
     )
 }
 
