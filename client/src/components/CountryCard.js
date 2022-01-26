8e111e8a5a9cf0e0ecc0dca7e990dd9d4302c41b
garyb6 <gary.bennett6@outlook.com>
2022-01-26 10:33:38 +0000

    removed container, card now takes over what will be grid box

--- client/src/components/CountryCard.js
+++ client/src/components/CountryCard.js
@@ -6,7 +6,6 @@ import "./CountryCard.css"
 const CountryCard = ({ selectedCountry, countryInfo, handleClose }) => {
     return (
 
-        <Container>
                 <Card  text="light" bg="dark" border="dark" style={{width: '100%', height: '100%'}}>
                 <CloseButton variant="white" onClick={handleClose} aria-label="hide"/>
                     <Card.Body>
@@ -16,7 +15,6 @@ const CountryCard = ({ selectedCountry, countryInfo, handleClose }) => {
                                     </Card.Text>
                     </Card.Body>
                 </Card>
-        </Container>
     )
 }
 
